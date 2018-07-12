a791e7bfba7b93a6cc7d8b9af70d64010c889a44
archit2693 <archit2693.aj@gmail.com>
2018-07-12 16:41:42 +0530

    update container views

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 Rails.application.config.assets.precompile += %w( navbar.js )
 Rails.application.config.assets.precompile += %w( container_host.scss )
+Rails.application.config.assets.precompile += %w( container_host.js )
